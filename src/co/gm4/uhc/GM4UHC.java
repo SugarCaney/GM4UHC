48a095061dcdceb46a63c39b1229be9b3e41784a
MrSugarCaney <MrSugarCaney@gmail.com>
2015-10-25 17:18:46 +0100

    Added TeamMananager to main class plus testing git script.

--- src/co/gm4/uhc/GM4UHC.java
+++ src/co/gm4/uhc/GM4UHC.java
@@ -2,11 +2,18 @@ package co.gm4.uhc;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import co.gm4.uhc.team.TeamManager;
+
 /**
  * @author MrSugarCaney
  */
 public class GM4UHC extends JavaPlugin {
 
+	/**
+	 * Instance managing the creation and storage of teams.
+	 */
+	private TeamManager teamManager = new TeamManager();
+	
 	@Override
 	public void onEnable() {
 		saveDefaultConfig();
@@ -17,6 +24,10 @@ public class GM4UHC extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		System.out.println("[GM4UHC] plugin has been disabled!");
+	}
+	
+	public TeamManager getTeamManager() {
+		return teamManager;
 	}
 
 }
